348322040ede8886753b63a3f406be08f5f7c4ef
tise.c <tiseheaini@gmail.com>
2015-05-26 15:17:30 +0800

    更改头像设置，使用 tuandui.im 的资源

--- app/models/user.js
+++ app/models/user.js
@@ -250,6 +250,7 @@ UserSchema.method('toJSON', function() {
         firstName: this.firstName,
         lastName: this.lastName,
         username: this.username,
+        email: this.email,
         displayName: this.displayName,
         avatar: this.avatar
     };
